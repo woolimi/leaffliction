696180fb340f9fd39d7731da5e41ff336a53b285
Woolim Park <woolimi91@naver.com>
2024-07-30 18:06:31 +0200

    chore: distribution code norminated

--- 01.Distribution.py
+++ 01.Distribution.py
@@ -9,6 +9,7 @@ import numpy as np
 
 IMAGE_FOLDER = "images"
 
+
 def download_and_extract_zip(url, extract_to='.'):
     """
     Download and extract a ZIP file from a given URL.
@@ -19,36 +20,49 @@ def download_and_extract_zip(url, extract_to='.'):
         zip_ref.extractall(extract_to)
     print("Download and extraction complete.")
 
+
 def analyze_images(directory):
     """
-    Analyze images in the directory and return a dictionary of counts per plant type.
+    Analyze images in the directory and
+    return a dictionary of counts per plant type.
     """
     plant_counts = Counter()
 
-    # Traverse through the directory and count the number of images in each subdirectory
+    # Traverse through the directory and
+    # count the number of images in each subdirectory
     for root, dirs, files in os.walk(IMAGE_FOLDER):
         for dirname in dirs:
             if not dirname.lower().startswith(directory.lower()):
                 continue
             dir_path = os.path.join(root, dirname)
-            num_images = len([file for file in os.listdir(dir_path) if file.lower().endswith(('.png', '.jpg', '.jpeg'))])
+            num_images = len([
+                file for file in os.listdir(dir_path) if file.lower().endswith(
+                    ('.png', '.jpg', '.jpeg')
+                )
+            ])
             plant_counts[dirname] = num_images
     return plant_counts
 
+
 def plot_pie_chart_and_bar_chart(data, title):
     """
-    Plot a pie chart and a bar chart side by side for the given data with consistent colors.
+    Plot a pie chart and a bar chart side by side
+    for the given data with consistent colors.
     """
-    # Get a list of colors from a colormap
-    colormap = plt.get_cmap("tab20") # Get a colormap from matplotlib
-    colors = colormap(np.linspace(0, 1, len(data))) # Sample the colormap to get enough colors
+
+    # Get a colormap from matplotlib
+    colormap = plt.get_cmap("tab20")
+
+    # Sample the colormap to get enough colors
+    colors = colormap(np.linspace(0, 1, len(data)))
 
     # Create a subplot with 1 row and 2 columns
     fig, axs = plt.subplots(1, 2, figsize=(16, 8))
 
     # Pie chart
     axs[0].pie(data.values(), autopct='%1.1f%%', startangle=140, colors=colors)
-    axs[0].axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    # Equal aspect ratio ensures that pie is drawn as a circle.
+    axs[0].axis('equal')
 
     # Bar chart
     axs[1].bar(data.keys(), data.values(), color=colors)
@@ -58,15 +72,16 @@ def plot_pie_chart_and_bar_chart(data, title):
 
     # Add a main title to the figure at the top left corner
     fig.suptitle(f'{title} class distribution', fontsize=16, ha='left', x=0.1)
-    
+
     # Adjust layout
-    plt.tight_layout(rect=[0, 0, 1, 0.95]) 
+    plt.tight_layout(rect=[0, 0, 1, 0.95])
 
     # Save the combined chart as a PNG file
     title = title.lower()
     plt.savefig(f'{title}_combined_chart.png')
     plt.close()
     print(f'Combined chart saved as {title}_combined_chart.png ✨')
+
 
 def main(directory, url):
     """
@@ -79,10 +94,10 @@ def main(directory, url):
         download_and_extract_zip(url, IMAGE_FOLDER)
     else:
         print("Directory already exists, skip downloading...")
-    
+
     # Analyze images
     plant_counts = analyze_images(directory)
-    
+
     if not plant_counts:
         print(f"No class found in the '{directory}' directory.")
         return
@@ -90,14 +105,18 @@ def main(directory, url):
     # Generate charts
     plot_pie_chart_and_bar_chart(plant_counts, os.path.basename(directory))
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="A program to analyze plant images and generate charts.")
+    parser = argparse.ArgumentParser(
+        description="A program to analyze plant images and generate charts."
+    )
 
     # Adding argument for the directory
     parser.add_argument(
-        'directory', 
+        'directory',
         type=str,
-        help='The directory to store extracted images and save the charts (ex: 01.Distribution Apple, Grape)'
+        help='The directory to store extracted images and save the charts \
+        (ex: 01.Distribution Apple, Grape)'
     )
 
     # Parsing the arguments
