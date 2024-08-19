7e1e2999e2f27bb4830a2fbb2a9154dd20000783
jungmoo cheon <developer.cheon@gmail.com>
2024-08-19 23:16:12 +0200

    wip: dataset load

--- 04.Classification.py
+++ 04.Classification.py
@@ -3,21 +3,55 @@ import torch.nn as nn
 import torch.optim as optim
 
 from torch.utils.data import DataLoader, random_split
+from torchvision import datasets, transforms
+from lib.images import IMAGE_FOLDER
 
 """
 TODO:
-    - load dataset
     - eval model
     - save model
-    - visualize
+    - visualize output
 """
+# Set hyperparameters
+lr = 0.001
+batch_size = 32
+epochs = 30
 
+
+# Load Dataset
+transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+    # image = (image - mean) / std, range [-1, 1]
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+])
+
+dataset = datasets.ImageFolder(root=IMAGE_FOLDER, transform=transform)
+
+NUM_CLASSES = len(dataset.classes)
+# Get the mapping of class names to their corresponding labels
+class_to_idx = dataset.class_to_idx
+
+print(f"Number of classes: {NUM_CLASSES}")
+print(f"Class to index mapping: {class_to_idx}")
+print("\n", dataset)
+
+train_size = int(0.8 * len(dataset))
+val_size = len(dataset) - train_size
+train_data, val_data = random_split(dataset, [train_size, val_size])
+
+train_loader = DataLoader(train_data, batch_size, shuffle=True)
+val_loader = DataLoader(val_data, batch_size, shuffle=False)
+print(f"Dataset has been loaded - batch size:{batch_size}", end=" ")
+print(f"train_data: {len(train_loader)}, val_data: {len(val_loader)}")
+
+# Define model
 class LeafClassifier(nn.Module):
     """
     Leaf Classifier that classifies the type of disease specified in the leaf
 
-    Input: leaf Image, shape of (256, 256, 3)
-    Label: a type of disease specified in the leaf (black rot, Esca, spot, and healthy)
+    Inputs: leaf Images, shape of (256, 256, 3)
+    Labels: types of disease specified in the leaf
     """
     def __init__(self):
         super(LeafClassifier, self).__init__()
@@ -28,8 +62,8 @@ class LeafClassifier(nn.Module):
 
         self.pool = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(256 * 15 * 15, 512)
-        self.fc2 = nn.Linear(512, 4)
+        self.fc1 = nn.Linear(256 * 14 * 14, 512)
+        self.fc2 = nn.Linear(512, NUM_CLASSES)
 
         self.dropout = nn.Dropout(0.5)
 
@@ -44,44 +78,30 @@ class LeafClassifier(nn.Module):
         x = self.pool(x)
 
         # Flatten tensor
-        x = x.view(-1, 256 * 15 * 15)
+        x = x.view(-1, 256 * 14 * 14)
 
         x = torch.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
         return x
 
-# Set hyperparameters
-lr = 0.001
-batch_size = 32
-epochs = 30
-
-
-# Dataset
-dataset = # TODO: load dataset 
-train_size = int(0.8 * len(dataset))
-val_size = int(dataset) - train_size
-train_data, val_data = random_split(dataset, [train_size, val_size])
-
-train_loader = DataLoader(train_data, batch_size, shuffle=True)
-val_loader = DataLoader(val_data, batch_size, shuffle=False)
-
-
 # Loss and Optimizer
 model = LeafClassifier() 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=lr)
 
-
 # Train model
+print(f"\nStart training model - epochs:{epochs}")
 for epoch in range(epochs):
     model.train()
     running_loss = 0.0
-    for inputs, labels in train_loader:
+    for i, (inputs, labels) in enumerate(train_loader):
         optimizer.zero_grad()
         outputs = model(inputs)
+        # print(inputs.shape, outputs.shape, labels.shape, labels)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
-    print(f"Epoch [{epoch + 1}/{epochs}] loss - {running_loss/len(train_loader):.4f}")
+        # print(f"Example: {i + 1}/{len(train_loader)}")
+    print(f"Epoch [{epoch + 1}/{epochs}] - loss: {running_loss/len(train_loader):.4f}")
