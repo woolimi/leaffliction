1df1b8f8a62f63701864b4dc5038fd3fe8c5fc7a
jmcheon <developer.cheon@gmail.com>
2024-08-20 18:46:28 +0200

    wip: tensorboard

--- 04.Classification.py
+++ 04.Classification.py
@@ -3,19 +3,19 @@ import torch.nn as nn
 import torch.optim as optim
 
 from torch.utils.data import DataLoader, random_split
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
 from torchvision import datasets, transforms
 from lib.images import IMAGE_FOLDER
 
 """
 TODO:
-    - eval model
-    - save model
     - visualize output
 """
 # Set hyperparameters
-lr = 0.001
-batch_size = 32
-epochs = 30
+lr = 0.01
+batch_size = 256
+epochs = 10
 
 
 # Load Dataset
@@ -43,6 +43,7 @@ train_data, val_data = random_split(dataset, [train_size, val_size])
 train_loader = DataLoader(train_data, batch_size, shuffle=True)
 val_loader = DataLoader(val_data, batch_size, shuffle=False)
 print(f"Dataset has been loaded - batch size:{batch_size}", end=" ")
+
 print(f"train_data: {len(train_loader)}, val_data: {len(val_loader)}")
 
 # Define model
@@ -90,18 +91,64 @@ model = LeafClassifier()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=lr)
 
-# Train model
-print(f"\nStart training model - epochs:{epochs}")
-for epoch in range(epochs):
-    model.train()
+#
+def train_one_epoch(epoch_index, tb_writer):
     running_loss = 0.0
+    last_loss = 0.0
+
     for i, (inputs, labels) in enumerate(train_loader):
         optimizer.zero_grad()
         outputs = model(inputs)
-        # print(inputs.shape, outputs.shape, labels.shape, labels)
         loss = criterion(outputs, labels)
+        # print(inputs.shape, outputs.shape, labels.shape)
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
-        # print(f"Example: {i + 1}/{len(train_loader)}")
-    print(f"Epoch [{epoch + 1}/{epochs}] - loss: {running_loss/len(train_loader):.4f}")
+        print(f"\tBatch [{i + 1}] - loss {running_loss}")
+        if i % batch_size == batch_size - 1:
+            last_loss = running_loss / batch_size
+            print(f"batch {i + 1} loss: {last_loss}")
+            tb_x = epoch_index * len(train_loader) + i + 1
+            tb_writer.add_scalar("Loss/train", last_loss, tb_x)
+            running_loss = 0
+
+    last_loss = running_loss / len(train_loader)
+    tb_x = epoch_index * len(train_loader) + i + 1
+    tb_writer.add_scalar("Loss/train", last_loss, tb_x)
+    return last_loss
+
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+writer = SummaryWriter(f"./runs/leaf_trainer_{timestamp}")
+epoch_number = 0
+best_vloss = 1_000_000.
+
+# 
+print(f"\nStart training model - epochs:{epochs}")
+for epoch in range(epochs):
+    print(f"Epoch [{epoch + 1}/{epochs}]")
+
+    # train
+    model.train(True)
+    avg_loss = train_one_epoch(epoch_number, writer)
+
+    # validation
+    model.eval()
+    running_vloss = 0.0
+    with torch.no_grad():
+        for i, (vinputs, vlabels) in enumerate(val_loader):
+            voutputs = model(vinputs)
+            vloss = criterion(voutputs, vlabels)
+            running_vloss += vloss
+    avg_vloss = running_vloss / (i + 1)
+    print(f"LOSS train {avg_loss:.4f} val {avg_vloss:.4f}")
+
+    # Tensorboard
+    writer.add_scalars("Training vs. Validation Loss", 
+                    {"Training": avg_loss, "Validation": avg_vloss},
+                    epoch_number + 1)
+    writer.flush()
+
+    if avg_vloss < best_vloss:
+        best_vloss = avg_vloss
+        torch.save(model.state_dict(), f"./model_{timestamp}_{epoch_number}")
+    epoch_number += 1
